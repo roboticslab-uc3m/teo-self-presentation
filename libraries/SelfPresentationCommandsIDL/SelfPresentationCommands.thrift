04d083be8180b674f42538cf04633e64f7ce60ed
Bartek Łukawski <bwmn.peter@gmail.com>
2021-11-15 21:50:35 +0100

    Mark motion commands as `oneway`

--- libraries/SelfPresentationCommandsIDL/SelfPresentationCommands.thrift
+++ libraries/SelfPresentationCommandsIDL/SelfPresentationCommands.thrift
@@ -2,17 +2,17 @@ namespace yarp roboticslab
 
 service SelfPresentationCommandsIDL
 {
-    void doGreet();
-    void doHoming();
-    void doExplanation1();
-    void doExplanation2();
-    void doExplanation3();
-    void doExplanationHead();
-    void doExplanationRightPC();
-    void doExplanationLeftPC();
-    void doExplanationInsidePC();
-    void doExplanationHDD();
-    void doExplanationSensors();
+    oneway void doGreet();
+    oneway void doHoming();
+    oneway void doExplanation1();
+    oneway void doExplanation2();
+    oneway void doExplanation3();
+    oneway void doExplanationHead();
+    oneway void doExplanationRightPC();
+    oneway void doExplanationLeftPC();
+    oneway void doExplanationInsidePC();
+    oneway void doExplanationHDD();
+    oneway void doExplanationSensors();
     bool checkMotionDone();
     bool stop();
 }
