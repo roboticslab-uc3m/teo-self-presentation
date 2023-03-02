46d61d123602b807016cc1f0e28a2d1cb4cad81d
Raúl de Santos Rico <rasantos@it.uc3m.es>
2023-03-02 17:29:37 +0100

    added new doExplanation4() function to SelfPresentationCommands.thrift

--- libraries/SelfPresentationCommandsIDL/SelfPresentationCommands.thrift
+++ libraries/SelfPresentationCommandsIDL/SelfPresentationCommands.thrift
@@ -7,6 +7,7 @@ service SelfPresentationCommandsIDL
     oneway void doExplanation1();
     oneway void doExplanation2();
     oneway void doExplanation3();
+    oneway void doExplanation4();
     oneway void doExplanationHead();
     oneway void doExplanationRightPC();
     oneway void doExplanationLeftPC();
